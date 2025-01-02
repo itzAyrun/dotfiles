d54b489ead3f578c1129ab52d05d2dd599164664
itzAyrun <ayrun3412@gmail.com>
2025-01-02 16:44:23 +0530

    Add keymap to toggle presence
    Leader+l should toggle on/off the discord rich presence

--- nvim/.config/nvim/lua/plugins/discord.lua
+++ nvim/.config/nvim/lua/plugins/discord.lua
@@ -2,5 +2,13 @@ return {
 	"vyfor/cord.nvim",
 	build = "./build || .\\build",
 	event = "VeryLazy",
-	opts = {},
+	config = function ()
+        require("cord").setup()
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader>l",
+            ":CordTogglePresence<CR>",
+            { noremap = true, silent = true }
+        )
+	end
 }
