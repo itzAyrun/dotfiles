ad248f9f0804adb59428abf2554ab622c25d1640
itzAyrun <ayrun3412@gmail.com>
2025-01-21 14:29:33 +0530

    Force commit

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -1,55 +1,53 @@
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
     local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
     if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-        { out, "WarningMsg" },
-        { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
     end
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Neovim opts
-vim.g.mapleader = " "
-vim.o.background = "dark"
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.autoindent = true
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.smarttab = true
-vim.opt.softtabstop = 4
-vim.opt.mouse = "a"
-vim.cmd([[set guicursor=n-v-c-i:block]])  -- Set the cursor to block for all modes
-vim.opt.clipboard:append('unnamedplus')
-vim.opt.swapfile = false
-
--- Toggle search highlighting
-local function toggle_highlight()
-    if vim.o.hlsearch then
-        vim.o.hlsearch = false
-        -- print("Search highlighting disabled")
-    else
-        vim.o.hlsearch = true
-        -- print("Search highlighting enabled")
-    end
-end
-
--- Map the toggle highlight fn to a keybinding and make it global
-vim.api.nvim_set_keymap("n", "<leader>h", ":lua toggle_highlight()<CR>", { noremap = true, silent = true })
-_G.toggle_highlight = toggle_highlight
-
--- Setup lazy.nvim
 require("lazy").setup({
-	spec = {
-		-- import your plugins
-		{ import = "plugins" },
-	},
+    spec = {
+        -- add LazyVim and import its plugins
+        { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+        -- import/override with your plugins
+        { import = "plugins" },
+    },
+    defaults = {
+        -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+        -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+        lazy = false,
+        -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+        -- have outdated releases, which may break your Neovim install.
+        version = false, -- always use the latest git commit
+        -- version = "*", -- try installing the latest stable version for plugins that support semver
+    },
+    install = { colorscheme = { "tokyonight", "habamax" } },
+    checker = {
+        enabled = true, -- check for plugin updates periodically
+        notify = false, -- notify on update
+    }, -- automatically check for plugin updates
+    performance = {
+        rtp = {
+            -- disable some rtp plugins
+            disabled_plugins = {
+                "gzip",
+                -- "matchit",
+                -- "matchparen",
+                -- "netrwPlugin",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin",
+            },
+        },
+    },
 })
--- nvim/.config/nvim/lua/plugins/gitignore.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "wintermute-cell/gitignore.nvim",
-    keys = {
-        { "<leader>gi", ":Gitignore<CR>", desc = "Generate .gitignore" },
-    },
-    config = function()
-        require("gitignore")
-    end,
-}
