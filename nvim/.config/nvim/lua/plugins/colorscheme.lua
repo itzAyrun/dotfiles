8c2e12389f5bd446eafbd2a38c43ffac3f4dbeb4
luvxrk <ayrun3412@gmail.com>
2025-02-21 11:03:07 +0530

    Update colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,98 @@
+return {
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = function()
+                require("catppuccin").load()
+            end,
+        },
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        opts = {
+            no_italic = true,
+            term_colors = true,
+            transparent_background = true,
+            styles = {
+                comments = {},
+                conditionals = {},
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+            },
+            color_overrides = {
+                mocha = {
+                    base = "#000000",
+                    mantle = "#000000",
+                    crust = "#000000",
+                },
+            },
+            integrations = {
+                telescope = {
+                    enabled = true,
+                    style = "nvchad",
+                },
+                dropbar = {
+                    enabled = true,
+                    color_mode = true,
+                },
+            },
+        },
+    },
+}
+
+-- return {
+--     "cdmill/neomodern.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         require("neomodern").setup({
+--             theme = "roseprime",
+--         })
+--         require("neomodern").load()
+--     end,
+-- }
+
+-- return {
+--     "rebelot/kanagawa.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         require("kanagawa").setup({
+--             transparent = true,
+--         })
+--         vim.cmd("colorscheme kanagawa")
+--     end,
+-- }
+
+-- return {
+--     "EdenEast/nightfox.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         vim.cmd("colorscheme carbonfox")
+--     end,
+-- }
+
 -- return {
 --     {
 --         "LazyVim/LazyVim",
 --         opts = {
---             colorscheme = function()
---                 require("catppuccin").load()
---             end,
+--             colorscheme = "catppuccin",
 --         },
 --     },
 --     {
 --         "catppuccin/nvim",
 --         name = "catppuccin",
---         priority = 1000,
 --         opts = {
---             no_italic = true,
 --             term_colors = true,
 --             transparent_background = false,
 --             styles = {
@@ -48,83 +128,3 @@
 --         },
 --     },
 -- }
-
--- return {
---     "cdmill/neomodern.nvim",
---     lazy = false,
---     priority = 1000,
---     config = function()
---         require("neomodern").setup({
---             theme = "roseprime",
---         })
---         require("neomodern").load()
---     end,
--- }
-
--- return {
---     "rebelot/kanagawa.nvim",
---     lazy = false,
---     priority = 1000,
---     config = function()
---         require("kanagawa").setup({
---             transparent = true,
---         })
---         vim.cmd("colorscheme kanagawa")
---     end,
--- }
-
--- return {
---     "EdenEast/nightfox.nvim",
---     lazy = false,
---     priority = 1000,
---     config = function()
---         vim.cmd("colorscheme carbonfox")
---     end,
--- }
-
-return {
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "catppuccin",
-        },
-    },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        opts = {
-            term_colors = true,
-            transparent_background = false,
-            styles = {
-                comments = {},
-                conditionals = {},
-                loops = {},
-                functions = {},
-                keywords = {},
-                strings = {},
-                variables = {},
-                numbers = {},
-                booleans = {},
-                properties = {},
-                types = {},
-            },
-            color_overrides = {
-                mocha = {
-                    base = "#000000",
-                    mantle = "#000000",
-                    crust = "#000000",
-                },
-            },
-            integrations = {
-                telescope = {
-                    enabled = true,
-                    style = "nvchad",
-                },
-                dropbar = {
-                    enabled = true,
-                    color_mode = true,
-                },
-            },
-        },
-    },
-}
